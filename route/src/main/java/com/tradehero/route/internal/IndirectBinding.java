d5df54ae4c4e21983f1ff5d1ae98aa22db45bbc9
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-12-04 17:41:18 +0800

    Documentation for IndirectBinding

--- route/src/main/java/com/tradehero/route/internal/IndirectBinding.java
+++ route/src/main/java/com/tradehero/route/internal/IndirectBinding.java
@@ -1,8 +1,18 @@
 package com.tradehero.route.internal;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 class IndirectBinding extends FieldBinding {
+  final Map<String, BundleType> typeMap = new LinkedHashMap<String, BundleType>();
   final String creatorName;
 
+  /**
+   * Binding for custom type, also see {@link com.tradehero.route.internal.BundleableBinding}
+   * Example: {@code @RouteProperty com.example.SomeType val } has
+   *  - name: val
+   *  - creatorName: com.example.SomeType
+   */
   public IndirectBinding(String name, String creatorName) {
     super(name);
     this.creatorName = creatorName;
